e6c83f98ab781a460080e5a620da7ed07e7371ee
Ksi0Na <57129186+Ksi0Na@users.noreply.github.com>
2021-01-14 18:14:45 +0300

    Add files via upload

---
+++ 13_LR5-2_MolchanovaOV.asm
@@ -0,0 +1,78 @@
+format MZ
+entry code_seg:start
+stack 200h
+
+segment data_seg
+len_of_input_with_1 db 7,0
+input db 7 dup(0)
+password db '19U061', '$'
+response_error db 0xa, 0xd, 'Error: Exceeded the limit of attempts', '$'
+response_correct db 0xa, 0xd, 'Password is correct', '$'
+enter_str db 0xa, 0xd, '$'
+attempts db 0xa, 0xd, 'Enter the password your attempts is limited. Attempts: ', '$'
+
+segment code_seg
+start:
+mov ax, data_seg
+mov ds, ax
+mov es, ax
+
+mov bl, 0x33
+attempt:
+	mov dx, attempts
+	mov ah, 0x9
+	int 0x21
+	
+	xor dx, dx
+	mov dl, bl
+	mov ah, 0x2
+	int 0x21
+	
+	xor dx, dx
+	xor ax, ax
+	
+	call print_enter
+	
+	mov dx, len_of_input_with_1
+	mov ah, 0x0A
+	int 0x21
+
+	mov di, input
+	mov si, password
+
+	mov cx, 6
+	repe cmpsb 
+	jnz _incorrect
+	jmp _correct
+
+_incorrect:
+	dec bl
+	cmp bl, 0x30
+	jne attempt
+	mov dx, response_error
+	mov ah, 0x9
+	int 0x21
+
+	call print_enter
+
+	mov ah, 0x4C
+	xor dx, dx
+	int 0x21
+
+_correct:
+	mov dx, response_correct
+	mov ah, 0x9
+	int 0x21
+	mov ah, 0x4C
+	xor dx, dx
+	int 0x21
+
+print_enter:
+	push dx
+	mov ah,0x9
+	mov dx, enter_str
+	int 0x21
+	pop dx
+	
+	ret
+
